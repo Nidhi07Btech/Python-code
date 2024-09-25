3036010f6ebf9eb6f008263e6792896799d6bd95
Nidhi Agrawal <nidhiagrawal426530@gmail.com>
2024-09-25 12:14:21 +0530

    initiate change

--- Operator.py
+++ Operator.py
@@ -11,9 +11,24 @@ print(a / b)
 print(a % b) # remainder
 print(a ** b) # a^b
 
-# Relational operator
+# Relational  operator or comparision operator
 # == , != , > , < , >= , <=
 
 A = 50
 B = 20
 
+print(A == b) # False
+print(A != B) # True
+print(A > B) # True
+print(A < B) # False
+print(A >= B) # True
+print(A <= B) # False
+
+# Assignment operator
+# = , += , -= , *= , /= , %= , **= 
+
+num = 10
+num = num + 10 # 10 + 10 => 20
+
+# Logocal operator
+# not , and , or
